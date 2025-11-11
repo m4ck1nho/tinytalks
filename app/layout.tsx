5541b455a49997f0dbeadf80c4a7e3c5d9cfc0e7
m4ck1nho <gkdnzkrks@gmail.com>
2025-11-12 01:13:13 +0300

    Add Vercel Web Analytics to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 import { HtmlLangWrapper } from '@/components/shared/HtmlLangWrapper';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -43,6 +44,7 @@ export default function RootLayout({
             {children}
           </HtmlLangWrapper>
         </LanguageProvider>
+        <Analytics />
       </body>
     </html>
   );
