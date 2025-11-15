cc1158a2019de853b7d3f15de12a8dbad2cdf0fc
m4ck1nho <gkdnzkrks@gmail.com>
2025-11-15 15:40:26 +0300

    fix: Replace any with unknown in StructuredData type

--- components/shared/StructuredData.tsx
+++ components/shared/StructuredData.tsx
@@ -1,13 +1,14 @@
 import Script from 'next/script';
 
 interface StructuredDataProps {
-  data: Record<string, any>;
+  data: Record<string, unknown>;
 }
 
 export function StructuredData({ data }: StructuredDataProps) {
+  const type = data['@type'] as string || 'unknown';
   return (
     <Script
-      id={`json-ld-${data['@type']}`}
+      id={`json-ld-${type}`}
       type="application/ld+json"
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
     />
