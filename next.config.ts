5240acbb3bf802d3783dcb02e179c2e9d69b28aa
m4ck1nho <gkdnzkrks@gmail.com>
2025-12-27 17:22:55 +0300

    Allow all https images in Next.js optimizer

--- next.config.ts
+++ next.config.ts
@@ -31,39 +31,56 @@ const nextConfig: NextConfig = {
   // Source: tinytalks.pro/*  Destination: https://www.tinytalks.pro/$1  (301)
   images: {
     remotePatterns: [
+      // Own domain (for absolute URLs)
+      {
+        protocol: 'https',
+        hostname: 'www.tinytalks.pro',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tinytalks.pro',
+      },
+      // Supabase storage
       {
         protocol: 'https',
         hostname: '**.supabase.co',
-        port: '',
         pathname: '/storage/v1/object/public/**',
       },
       {
         protocol: 'http',
         hostname: '**.supabase.co',
-        port: '',
         pathname: '/storage/v1/object/public/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.ufs.sh',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**.ufs.sh',
-        port: '',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: '**.supabase.in',
-        port: '',
         pathname: '/storage/v1/object/public/**',
+      },
+      // Uploadthing
+      {
+        protocol: 'https',
+        hostname: '**.ufs.sh',
+      },
+      {
+        protocol: 'http',
+        hostname: '**.ufs.sh',
       },
       {
         protocol: 'https',
+        hostname: '**.uploadthing.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+      // Stock photo services
+      {
+        protocol: 'https',
         hostname: '**.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
       },
       {
         protocol: 'https',
@@ -71,11 +88,24 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
         hostname: '**.imgur.com',
       },
       {
         protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
         hostname: '**.cloudinary.com',
+      },
+      // Allow any https image as fallback
+      {
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
